262f5fce8d0fd08717ca8b77d9011043179099d2
abdul17097 <abdul17097@gmail.com>
2024-05-02 23:31:18 -0700

    create Header section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="px-[2rem]">
+      <Header />
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { IoIosArrowDown, IoIosArrowRoundForward } from "react-icons/io";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import logo from "../assest/logo.png";
+const Header = () => {
+  return (
+    <nav className="h-[5.5rem] w-full border flex justify-between items-center pr-[1.5rem]">
+      <div className="">
+        <img src={logo} alt="" className="h-[4rem] w-[4rem]" />
+      </div>
+      <div className="flex items-center gap-[2.5rem] h-[2.5rem]">
+        <div className="flex items-center gap-[3rem]">
+          <div className="flex items-center gap-[5px]">
+            <span className="">Servieces</span>
+            <IoIosArrowDown />
+          </div>
+          <div className="">
+            <span className="">Commercial</span>
+          </div>
+        </div>
+        <div className="flex gap-[1.5rem]">
+          <div className="flex items-center gap-[4px]">
+            <span className="font-bold text-[18px]">login</span>
+            <IoIosArrowRoundForward />
+          </div>
+          <div className="">
+            <button className="bg-[#EBAD1E] focus:outline-none px-[2rem] py-[.5rem] rounded-full text-black text-[14px]">
+              Book Service
+            </button>
+          </div>
+        </div>
+        <div className="text-3xl">
+          <MdOutlineShoppingCart />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
